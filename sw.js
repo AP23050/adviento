7c7f1ffad66bd19b9124dd75a9bd223c37169cf2
Rodrigo Antonio Alvarado Perez <167836635+AP23050@users.noreply.github.com>
2025-12-12 16:07:02 -0600

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,9 @@
-const CACHE = "adviento-v1";
-const FILES = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
+const CACHE="adviento-v1";
+const FILES=["./","./index.html","./manifest.json"];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(FILES))
-  );
+self.addEventListener("install",e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
 });
-
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
+self.addEventListener("fetch",e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
